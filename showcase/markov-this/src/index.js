678487f1c2266a2823e5a6a56cc680c2f92b8549
raltamirano <rodrigo.altamirano@gmail.com>
2019-05-12 19:51:23 +0100

    Refactor to use markov-this NPM package

---
+++ showcase/markov-this/src/index.js
@@ -0,0 +1,13 @@
+import { 
+    Markov, 
+    Analyzer, 
+    TextLinesAnalyzer,
+    FullStopAnalyzer
+} from 'markov-this'
+
+export { 
+    Markov, 
+    Analyzer, 
+    TextLinesAnalyzer,
+    FullStopAnalyzer
+}
---
+++ showcase/markov-this/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry : "./src/index.js",
+    mode: "production",
+    output: {
+        filename: 'markov-this-showcase.js',
+        library: 'markov'
+    },    
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ]
+};
