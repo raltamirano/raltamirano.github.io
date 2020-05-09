cd907dc2880325a6d40c74a50aae6ef9ae3b8f3b
raltamirano <rodrigo.altamirano@gmail.com>
2020-05-09 12:35:11 -0300

    Add QR Play

---
+++ showcase/qr-play/service-worker.js
@@ -0,0 +1,87 @@
+"use strict";
+
+var version = 'v1::';
+var offlineFundamentals = [
+  '',
+  'index.html',
+  'js/app.js',
+  'js/jsQR.js'
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches
+
+      .open(version + 'fundamentals')
+      .then(function(cache) {
+        return cache.addAll(offlineFundamentals);
+      })
+      .then(function() {
+        console.log('WORKER: install completed');
+      })
+  );
+});
+
+
+self.addEventListener("fetch", function(event) {
+  if (event.request.method !== 'GET')
+    return;
+ 
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function(cached) {
+        var networked = fetch(event.request)
+          .then(fetchedFromNetwork, unableToResolve)
+          .catch(unableToResolve);
+        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+        return cached || networked;
+
+        function fetchedFromNetwork(response) {
+          var cacheCopy = response.clone();
+          console.log('WORKER: fetch response from network.', event.request.url);
+          caches
+            .open(version + 'pages')
+            .then(function add(cache) {
+              return cache.put(event.request, cacheCopy);
+            })
+            .then(function() {
+              console.log('WORKER: fetch response stored in cache.', event.request.url);
+            });
+          return response;
+        }
+
+        function unableToResolve () {
+          console.log('WORKER: fetch request failed in both cache and network.');
+          return new Response('<h1>Service Unavailable</h1>', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+              'Content-Type': 'text/html'
+            })
+          });
+        }
+      })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(function (keys) {
+        return Promise.all(
+          keys
+            .filter(function (key) {
+              return !key.startsWith(version);
+            })
+            .map(function (key) {
+              return caches.delete(key);
+            })
+        );
+      })
+      .then(function() {
+        console.log('WORKER: activate completed.');
+      })
+  );
+});
